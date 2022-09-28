b4f19c75c83abbc9817917de6b57735d59aea0a3
obl1v1on <erbarasuol@gmail.com>
2022-09-28 13:01:36 -0300

    bug fix

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn render_articles(articles: &Articles) {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let url = "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=abc29fcdb38f451b9e69aa34f22162e7";
+    let url = "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=<API_KEY>";
     let articles = get_articles(url)?;
 
     render_articles(&articles);
