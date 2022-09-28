ed09264312b164ed1ab8c248699e48d0d6f23215
obl1v1on <erbarasuol@gmail.com>
2022-09-28 11:17:12 -0300

    cli news first version

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use colour;
+use std::error::Error;
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct Articles {
+    articles: Vec<Article>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Article {
+    title: String,
+    url: String,
+}
+
+fn get_articles(url: &str) -> Result<Articles, Box<dyn Error>> {
+    let response = ureq::get(url).call()?.into_string()?;
+
+    let articles: Articles = serde_json::from_str(&response)?;
+
+    Ok(articles)
+}
+
+fn render_articles(articles: &Articles) {
+    for i in &articles.articles {
+        colour::dark_blue!("> {}\n", i.title);
+        colour::e_dark_magenta!("> {}\n", i.url);
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>>{
+    let url = "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=<API_KEY>";
+    let articles = get_articles(url)?;
+
+    render_articles(&articles);
+
+    Ok(())
+}
