37494967dff7d76f2218ef645db8e16c03854630
obl1v1on <erbarasuol@gmail.com>
2022-09-28 13:24:34 -0300

    added dotenv

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use colour::{dark_blue, e_dark_magenta};
+use dotenv::dotenv;
 use newsapi::{get_articles, Articles};
 use std::error::Error;
 
@@ -10,8 +11,15 @@ fn render_articles(articles: &Articles) {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let url = "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=<API_KEY>";
-    let articles = get_articles(url)?;
+    dotenv()?;
+
+    let api_key = std::env::var("API_KEY")?;
+
+    let url = format!(
+        "{}{}",
+        "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=", api_key
+    );
+    let articles = get_articles(&url)?;
 
     render_articles(&articles);
 
